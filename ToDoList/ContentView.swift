c9b49d84f08ac90e1914550c5ef14387567c2b30
Dhriti Burugupalli <burugupallidhriti@gmail.com>
2025-07-25 13:29:21 -0700

    Second view appearance completed

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("To Do List")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .padding(.all, 20.0)
+                Spacer()
+                Button {
+
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+                }
+
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
